5fc34340e4fc8b1065e77067c79ba1e141eb44f8
Ellison <mellcx@gmail.com>
2024-01-10 14:19:26 +0800

    Update diy-part1.sh
    fix onliner

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,17 +10,6 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-function git_sparse_clone() {
-branch="$1" rurl="$2" localdir="$3" && shift 3
-git clone -b $branch --depth=1 --filter=blob:none --sparse $rurl $localdir
-cd $localdir
-git sparse-checkout init --cone
-git sparse-checkout set $@
-mv -n $@ ../
-cd ..
-rm -rf $localdir
-}
-
 #sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 
 # 添加ssrp
@@ -43,7 +32,7 @@ git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 # git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 
 # 添加lucky
-git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
+git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 
 # 添加watchcat-plus
 git clone https://github.com/gngpp/luci-app-watchcat-plus.git package/luci-app-watchcat-plus
@@ -57,7 +46,9 @@ git clone https://github.com/gngpp/luci-app-watchcat-plus.git package/luci-app-w
 
 # 在线用户
 # svn export https://github.com/haiibo/packages/trunk/luci-app-onliner package/luci-app-onliner
-git_sparse_clone main "https://github.com/haiibo/packages" "packages" luci-app-onliner
+git clone https://github.com/haiibo/packages
+mv packages/luci-app-onliner package
+rm -rf packages
 sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/lean/default-settings/files/zzz-default-settings
 sed -i '$i uci commit nlbwmon' package/lean/default-settings/files/zzz-default-settings
 chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
